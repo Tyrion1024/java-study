c100f6b51a1ce3e70a0433f4a79699c721fd0f37
Tyrion1024 <623635445@qq.com>
2020-09-15 17:37:32 +0800

    fuchou update

--- test2/Test.java
+++ test2/Test.java
@@ -1,6 +1,6 @@
 public class Test {
-  public static void main (String[] args) {
-    int a = 1;
+  /*public static void main(String[] args) {
+   int a = 1;
     int b = 2;
     
     a = a ^ b;
@@ -8,5 +8,39 @@ public class Test {
     a = a ^ b;
     System.out.println(a);
     System.out.println(b);
+  }
+    int a = 2;
+    int b = 4;
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println(a / b);//除法得到的是商，只显示整数
+    System.out.println(a % b);//取余得到余数
+//整数相除只能得到整数，要想得到小数必须有浮点数的参与
+System.out.println(6.0/4);
+  }
+  public static void main(String[] args) {
+    //定义
+    int i = 10;
+    char c = 'A';//'A'值是65
+    int d = 'a';//'a'值是97
+    System.out.println(i+c);
+    System.out.println(i+d);
+    //char ch = i + c;char类型会被自动提升为int类型，所以进行以下操作
+    int j = i + c;
+    System.out.println(j);
+
+    //int k = 10+13.14;//13.14是double，不能用int；
+    double f = 10+13.14;
+    System.out.println(f);*/
+  public static void main(String[] args) {
+
+    System.out.println("it"+"heima");
+    System.out.println("it"+666);
+    System.out.println(666+"heima");
+    System.out.println("heima"+6+66);
+    System.out.println("heima"+6+66);
+    System.out.println(1+99+"年");
+
   }
 }
---
+++ test2/Yu.java
@@ -0,0 +1,6 @@
+public class Yu {
+    public static void main(String[] args) {
+        int i = 10;//把10赋值给int类型的变量i；
+        
+    }
+}
