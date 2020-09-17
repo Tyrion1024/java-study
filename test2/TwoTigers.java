a8a65327da8844f3256b8b8e6b98e7e79a65c8ad
Tyrion1024 <623635445@qq.com>
2020-09-17 10:52:34 +0800

    update

---
+++ test2/TwoTigers.java
@@ -0,0 +1,14 @@
+public class TwoTigers {
+    public static void main(String[] args) {
+        int weight1 = 180;
+        int weight2 = 200;
+        boolean b = weight1 == weight2 ?true : false;
+        System.out.println("b:" +b);
+        //System.out.println(weight1 == weight2);
+        //System.out.println("--------");
+       // System.out.println(weight1 > weight2);
+       // System.out.println("--------");
+       // System.out.println(weight1 < weight2);
+        //System.out.println("--------");
+    }
+}
